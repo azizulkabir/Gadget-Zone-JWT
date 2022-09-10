4c9659eedc0b251cc9237b071c1caadc4310582b
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-11 02:31:35 +0600

    Login form

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,9 +1,40 @@
+
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Login = () => {
     return (
         <div>
-            this is login  
+            <div >
+                <h3 className='text-center'>Please Login First</h3>
+                <div className='w-50 mx-auto text-left'>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="success" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+            </div>
+            <div className='text-center'>
+                <button className='btn btn-success'> Google SignIn </button>
+            </div>
         </div>
     );
 };
