636a6ea25d1cd0dae2777c4fa9c9ea3e0f8a4906
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-11 03:51:13 +0600

    sign out and handle user

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,12 +1,21 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 
 
-const Header = () => {
+
+const Header = () => { 
+    const [user] = useAuthState(auth);
+    const handleSignOut=(event)=>{
+        event.preventDefault();
+        signOut(auth);
+    }
     return (
         <div>
             <Navbar>
@@ -18,12 +27,21 @@ const Header = () => {
                         <Link style={{textDecoration: 'none'}} className='mx-1 text-success ' to='/about'>About</Link>
                         <Link style={{textDecoration: 'none'}} className='mx-1 text-success ' to='/products'>Products</Link>
                         <Link style={{textDecoration: 'none'}} className='mx-1 text-success ' to='/uploadproduct'>UploadProduct</Link>
-                        <Link style={{textDecoration: 'none'}} className='mx-1 text-success ' to='/login'>Login</Link>
+                        
                         
                     </Nav>
                     <Navbar.Collapse className="justify-content-end">
                         <Navbar.Text>
-                            user
+                           {
+                            user ? 
+                            <div>
+                                <span>{user.displayName}</span> <span ><img className='rounded-circle' src={user.photoURL} alt="" /></span> <span onClick={handleSignOut} className='btn btn-success'>LogOut</span>
+                            </div>
+                              :
+                            <div>
+                                <Link style={{textDecoration: 'none'}} className='mx-1 text-success ' to='/login'>Login</Link>
+                            </div>
+                           }
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Container>
