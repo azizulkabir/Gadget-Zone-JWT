0ec736d2f5661d2dfb6ad5cd3f64df854cd3ef6a
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-14 00:26:49 +0600

    upload form

--- src/Component/UploadProducts/UploadProducts.js
+++ src/Component/UploadProducts/UploadProducts.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const UploadProducts = () => {
     return (
         <div>
-            this is upload products 
+            <h3 className='text-center'>Upload Products:</h3>
+            <div className='w-50 mx-auto'>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Product Name: </Form.Label>
+                        <Form.Control type="text" name='name' placeholder="Enter product name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Product Price: </Form.Label>
+                        <Form.Control type="text" name='price' placeholder="Enter product price" />
+                    </Form.Group>
+                    <Button variant="success" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
