f22680874173cccffe8d5e6d5f3dbcc995ba8d6b
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-15 04:24:39 +0600

    card

--- src/Component/Product/Products.js
+++ src/Component/Product/Products.js
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
-   
+
 
 
     useEffect(() => {
         const url = 'http://localhost:5000/products';
         fetch(url)
-            .then(res=>res.json())
-            .then(data=>setProducts(data))
-        
+            .then(res => res.json())
+            .then(data => setProducts(data))
+
     }, [])
 
     return (
         <div className='row'>
-           {
-            products.map(product=>(<div className='col-4' key={product._id}> 
-                Name: {product.productName}<br></br>
-                Name: {product.productPrice}
-            </div>))
-           }
-           
+            {
+                products.map(product => (<div className='col-3 ' key={product._id}>
+                    <Card className='my-3' style={{ width: '15rem' }}>
+                        <Card.Body>
+                            <Card.Title>{product.productName}</Card.Title>
+                            <Card.Text>{product.productPrice}</Card.Text>
+                            <Button variant="success">Buy Now</Button>
+                        </Card.Body>
+                    </Card>
+                </div>))
+            }
+
         </div>
     );
 };
